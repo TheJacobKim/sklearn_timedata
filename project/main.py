e0094a681441b2760e768722813f263e0a10812e
JacobKim <hyk053@ucsd.edu>
2019-02-15 03:06:44 -0800

    Prints mean fit time

--- project/main.py
+++ project/main.py
@@ -10,107 +10,97 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 
-h = .02  # step size in the mesh
+f = open('result.txt','w')
 
 names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
          "Naive Bayes", "QDA"]
 
-classifiers = [
-    KNeighborsClassifier(3),
-    SVC(kernel="linear", C=0.025),
-    SVC(gamma=2, C=1),
-    GaussianProcessClassifier(1.0 * RBF(1.0)),
-    DecisionTreeClassifier(max_depth=5),
-    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    MLPClassifier(alpha=1),
-    AdaBoostClassifier(),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis()]
+scores = ['accuracy', 'precision', 'recall']
 
+classifiers = [
+    KNeighborsClassifier(),
+    SVC(),
+    SVC(),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(),
+    MLPClassifier()
+    # GaussianProcessClassifier(),future purposes
+    # AdaBoostClassifier(),
+    # GaussianNB(),
+    # QuadraticDiscriminantAnalysis()
+]
+
+# Set the parameters by cross-validation
+k_range = list(range(1, 31))
+n_range = list(range(1, 10))
+m_range = list(range(1, 3))
+#kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+tuned_parameters = [
+    {'n_neighbors': k_range},  # knn
+    {'kernel': ['rbf'], 'gamma': [1e-3, 1e-4], 'C': [1, 20, 100, 1000]},  # RBF SVM
+    {'kernel': ['linear'], 'C': [1, 20, 100, 1000]},  # Linear SVM"
+    {'max_depth': n_range},  # Decision Tree
+    {'max_depth': n_range, 'n_estimators': n_range, 'max_features': m_range},  # Random Forest
+    {'alpha': m_range},  # Neural Net
+    # {'kernel': kernel},  # Gaussian Process
+]
+
+# Create random dataset
 X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
                            random_state=1, n_clusters_per_class=1)
+
 rng = np.random.RandomState(2)
 X += 2 * rng.uniform(size=X.shape)
 linearly_separable = (X, y)
 
-datasets = [make_moons(noise=0.3, random_state=0),
+datasets = [make_moons(noise=0.3, random_state=1),
             make_circles(noise=0.2, factor=0.5, random_state=1),
             linearly_separable
             ]
 
-figure = plt.figure(figsize=(27, 9))
-i = 1
+dataset_num = 0
 # iterate over datasets
 for ds_cnt, ds in enumerate(datasets):
+    f.write("\n\n\nRandom data set #" + str(dataset_num))
+    dataset_num += 1
     # preprocess dataset, split into training and test part
     X, y = ds
     X = StandardScaler().fit_transform(X)
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size=.4, random_state=42)
-
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-
-    # just plot the dataset first
-    cm = plt.cm.RdBu
-    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-    if ds_cnt == 0:
-        ax.set_title("Input data")
-    # Plot the training points
-    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-               edgecolors='k')
-    # Plot the testing points
-    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,
-               edgecolors='k')
-    ax.set_xlim(xx.min(), xx.max())
-    ax.set_ylim(yy.min(), yy.max())
-    ax.set_xticks(())
-    ax.set_yticks(())
-    i += 1
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=42)
 
     # iterate over classifiers
+    i = 0
     for name, clf in zip(names, classifiers):
-        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-
-        # Plot the decision boundary. For that, we will assign a color to each
-        # point in the mesh [x_min, x_max]x[y_min, y_max].
-        if hasattr(clf, "decision_function"):
-            Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-        else:
-            Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
-
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
-        ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
-
-        # Plot the training points
-        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-                   edgecolors='k')
-        # Plot the testing points
-        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
-                   edgecolors='k', alpha=0.6)
-
-        ax.set_xlim(xx.min(), xx.max())
-        ax.set_ylim(yy.min(), yy.max())
-        ax.set_xticks(())
-        ax.set_yticks(())
-        if ds_cnt == 0:
-            ax.set_title(name)
-        ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
-                size=15, horizontalalignment='right')
+        grid = GridSearchCV(clf, tuned_parameters[i], cv=10, scoring='accuracy')
         i += 1
+        grid.fit(X_train, y_train)
 
-plt.tight_layout()
-plt.show()
+        # Best estimator overall
+        f.write("\n\n____________________________________________________________\n")
+        f.write("Best estimator\n")
+        f.write(str(grid.best_estimator_))
+
+        # What was the best param?
+        f.write("\n\nBest param\n")
+        f.write(str(grid.best_params_))
+
+        # Save params & mean fit time and compare
+        param_list = grid.cv_results_['params']
+        mean_fit_time = grid.cv_results_['mean_fit_time']
+        f.write("\n\nMean fit time\n")
+        for param, time in zip(param_list, mean_fit_time):
+            f.write(str(param) + ":  ")
+            f.write(str(time) + '\n')
+
+
+f.close()
